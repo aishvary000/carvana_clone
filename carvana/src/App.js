a261411a983ebc507572080916975539be54a811
prithuu05 <prithu.bansal05@gmail.com>
2023-03-27 14:17:13 +0530

    main page section

--- carvana/src/App.js
+++ carvana/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import MainHeader from './layouts/MainHeader';
+import Section from './layouts/Section';
 
 function App() {
   return (
     <div className="App">
       <MainHeader/>
+      <Section/>
     </div>
   );
 }
---
+++ carvana/src/layouts/Section.js
@@ -0,0 +1,37 @@
+import react from "react";
+
+import "./Section.css";
+import bg from '../assets/download.jpeg'
+
+const Section = () => {
+
+    return (<>
+    
+    <div className="main-section">
+    <div className="section-box">
+        <img className="section-img" src="https://www.carbravo.com/s3/uploads/US/homepage_woman_in_drivers_seat_lg_cd133524b8.jpg?imwidth=1440" alt="Loading" />
+    </div>
+    <div className="heading">
+        <h1>The cars are used.</h1>
+    </div>
+    <div className="heading-2">
+        <h1>The experience is</h1>
+    </div>
+    <div className="heading-3">
+        <h1>amazingly new.</h1>
+    </div>
+
+    <div className="inputArea">
+        <input type="text" />
+
+    </div>
+    </div>
+    
+    
+    
+    </>
+    )
+}
+
+
+export default Section;
